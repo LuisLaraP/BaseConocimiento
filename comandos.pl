406d1a18612999484911e31c69d00b0f102ab3ca
Luis Lara <luislpatino@gmail.com>
2017-10-05 19:54:44 -0500

    Agrega el comando borrarPropClase

--- comandos.pl
+++ comandos.pl
@@ -39,6 +39,18 @@ comando(nuevaPropClase(Nombre, Propiedad), Base, NuevaBase) :-
 		Base, NuevaBase
 	).
 
+% Elimina la propiedad especificada de la clase.
+%	Nombre - Nombre de la clase a modificar.
+%	Propiedad - Nombre de la propiedad a eliminar.
+comando(borrarPropClase(Nombre, Propiedad), Base, NuevaBase) :-
+	buscar(clase(Nombre, _, _, _), Base, clase(_, Padre, Props, Rels)),
+	eliminar(Propiedad, Props, NuevasProps),
+	reemplazar(
+		clase(Nombre, Padre, Props, Rels),
+		clase(Nombre, Padre, NuevasProps, Rels),
+		Base, NuevaBase
+	).
+
 % Agrega un nuevo objeto a la base de conocimiento. Versión simple.
 %	Nombre - Identificador para el objeto.
 %	Padre - Clase a la cual pertenece el nuevo objeto.
